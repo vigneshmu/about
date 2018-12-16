7ea4772c05780f4d8b92b59e41006ce347061512
vignesh <vignesh@abc.com>
2018-12-16 19:54:19 +0530

    Updates

---
+++ precache-manifest.9a445a0276e4c75074a88ce4f39027da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "f5eaa0121b76d5ec574d",
+    "url": "/profile/static/js/main.f5eaa012.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/profile/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "f5eaa0121b76d5ec574d",
+    "url": "/profile/static/css/main.b02c6ac7.chunk.css"
+  },
+  {
+    "revision": "842526cacd2de965be95e9fed8c47d5c",
+    "url": "/profile/index.html"
+  }
+];
